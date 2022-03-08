14d7fc3e3f392c373869d10a8b8c05b2226f083b
Fabio <fipsfabio95@gmail.com>
2022-03-08 15:49:43 +0100

    spostato in alto il sia multiplo di 3 che multiplo di 5 altrimenti non si leggeva

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ console.log('js ok')
 const grigliaNumerica = document.querySelector("#grigliaNumerica");
 
 for(let boxIndex = 1; boxIndex <= 100; boxIndex++){
+
+
+    if (boxIndex % 3 == 0 && boxIndex % 5 == 0) {
+
+        let element = `<div class="numberBox fizzbuzz">FizzBuzz</div>`;
+        console.log('Buzz');
+        grigliaNumerica.innerHTML += element;
+    }
    
-   
-    if (boxIndex % 3 == 0){
+    else if (boxIndex % 3 == 0){
         let element = `<div class="numberBox fizz">Fizz</div>`;
         console.log('Fizz');
         grigliaNumerica.innerHTML += element;
@@ -19,13 +26,6 @@ for(let boxIndex = 1; boxIndex <= 100; boxIndex++){
         grigliaNumerica.innerHTML += element;
     }
 
-    else if (boxIndex % 3 == 0 && boxIndex % 5 == 0) {
-
-        let element = `<div class="numberBox fizzbuzz">FizzBuzz</div>`;
-        console.log('Buzz');
-        grigliaNumerica.innerHTML += element;
-    }
-      
     else{
     let element = `<div class="numberBox">${boxIndex}</div>`;
     console.log(boxIndex);
@@ -33,10 +33,6 @@ for(let boxIndex = 1; boxIndex <= 100; boxIndex++){
 
     }
 
-
-
-   
-    
 }
 
 
